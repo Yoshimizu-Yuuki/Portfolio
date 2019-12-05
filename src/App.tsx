9ddaad63b81a95d251c26830906df77859a02a63
Yoshimizu-Yuuki <yoshimizuyuuki@gail.com>
2019-12-05 20:26:02 +0900

    完成版！

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Resume from './Resume';
+import Home from './Home';
+import Skill from './Skill';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/skill">
+            <Skill />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+/**
+     <Router>
+      <Link to="/">Home</Link>
+      <Link to="/resume">Resume</Link>
+    </Router>
+ */
+
+// import React from "react";
+// import Resume from './resume';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/resume">Resume</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         <Switch>
+//           <Route path="/resume">
+//             <Resume />
+//           </Route>
+//           <Route path="/">
+//             <App />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
